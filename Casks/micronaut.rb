5eb91c34497c67f00cbeaddba83556191a69e6e3
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-06-18 13:37:54 +0200

    Initial commit

---
+++ Casks/micronaut.rb
@@ -0,0 +1,12 @@
+cask 'micronaut' do
+  version '2.0.0.RC1'
+  sha256 '6fa474cf8d5b5bcb1d0f91c79bb784ed606027454b644a92c75834f30a47736e'
+
+  # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
+  url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
+  appcast 'https://github.com/micronaut-projects/micronaut-starter/releases.atom'
+  name 'Micronaut'
+  homepage 'https://micronaut.io/'
+
+  binary "mn-darwin-amd64-v#{version}/bin/mn"
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Micronaut's Homebrew Tap
+
+## Installation
+
+`brew install micronaut-projects/tap/micronaut`
+
+Or `brew tap micronaut-projects/tap` and then `brew install micronaut`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
