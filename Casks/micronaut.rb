8bff8f8a637b5a02f84203c2d8fd607e1a71bce4
graemerocher <graemerocher@users.noreply.github.com>
2020-06-22 13:38:47 +0000

    Update Micronaut cask to v2.0.0.RC2

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -1,6 +1,6 @@
 cask 'micronaut' do
-  version '2.0.0.RC1'
-  sha256 '6fa474cf8d5b5bcb1d0f91c79bb784ed606027454b644a92c75834f30a47736e'
+  version '2.0.0.RC2'
+  sha256 '2015ac6103f354b7c20e162093e51e9d6c509c90ad4b7e94f1a7d87356c9d8df'
 
   # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
   url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
