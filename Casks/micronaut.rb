82f18ab386a54d6d432600f4d41a16bd5453a2f0
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2020-10-22 09:55:06 +0200

    Update Micronaut cask to v2.1.2

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -1,12 +1,12 @@
 cask 'micronaut' do
-  version '2.1.1'
-  sha256 '54df9bdb5ef4d5e79c815e63e205b6ba2d7be4288c3ac585a39643bf173bf120'
+  version '2.1.2'
+  sha256 '24d5fa3599ddec5886cfe8a3e92c527dcf41f7045d3ec2dfe808c99d04be9022'
 
   # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
-  url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
-  appcast 'https://github.com/micronaut-projects/micronaut-starter/releases.atom'
+  url "micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
+  appcast 'micronaut-projects/micronaut-starter/releases.atom'
   name 'Micronaut'
-  homepage 'https://micronaut.io/'
+  homepage 'micronaut.io'
 
   binary "mn-darwin-amd64-v#{version}/bin/mn"
 end
