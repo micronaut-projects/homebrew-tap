521ba27a05bc7c875982aa842b49c1c56674cbe3
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2020-10-22 09:56:22 +0200

    Fix URLs

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -3,10 +3,10 @@ cask 'micronaut' do
   sha256 '24d5fa3599ddec5886cfe8a3e92c527dcf41f7045d3ec2dfe808c99d04be9022'
 
   # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
-  url "micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
-  appcast 'micronaut-projects/micronaut-starter/releases.atom'
+  url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
+  appcast 'https://github.com/micronaut-projects/micronaut-starter/releases.atom'
   name 'Micronaut'
-  homepage 'micronaut.io'
+  homepage 'https://micronaut.io'
 
   binary "mn-darwin-amd64-v#{version}/bin/mn"
 end
