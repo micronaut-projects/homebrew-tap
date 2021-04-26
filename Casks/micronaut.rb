b3a428cba80fbbc83dbce7110395619e1ffc13f6
micronaut-build <micronaut-build-account@grails.org>
2021-04-26 19:19:20 +0000

    Update Micronaut cask to v2.4.3

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -1,6 +1,6 @@
 cask 'micronaut' do
-  version '2.4.2'
-  sha256 '9e74596439688dd9ac96e3d098e5ec9ce59fc25c59e8744c548e8356741a832b'
+  version '2.4.3'
+  sha256 '9d95bdcd5a1c55df71a3eaf6accb76683a32e49549d9c9ae2ce127ef8d23f199'
 
   # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
   url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
