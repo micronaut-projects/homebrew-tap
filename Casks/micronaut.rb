3022344f14d853e5302f207e1d9588f0420ba45e
Tim Yates <tim.yates@gmail.com>
2024-04-26 16:05:59 +0100

    Update cask to support arm releases
    This needs to be in place before the starter release containing arm releases is attempted

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -1,11 +1,15 @@
 cask 'micronaut' do
-  version '4.4.1'
-  sha256 'a5b38ac0b63847331462dca9d5821d607ab56f28ad163e20c3237b22d49ee950'
+  arch arm: "aarch64", intel: "amd64"
 
-  # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
-  url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
+  version '4.4.1'
+
+  sha256 arm: 'a5b38ac0b63847331462dca9d5821d607ab56f28ad163e20c3237b22d49ee950',
+         intel: 'a5b38ac0b63847331462dca9d5821d607ab56f28ad163e20c3237b22d49ee950'
+
+  url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-#{arch}-v#{version}.zip"
+
   name 'Micronaut'
   homepage 'https://micronaut.io'
 
-  binary "mn-darwin-amd64-v#{version}/bin/mn"
+  binary "mn-darwin-#{arch}-v#{version}/bin/mn"
 end
