c1287bd6b7e14161e7b78c3363ed438e3ade0362
micronaut-build <micronaut-build-account@grails.org>
2024-11-13 20:58:17 +0000

    Update Micronaut cask to v4.7.0

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -1,10 +1,10 @@
 cask 'micronaut' do
   arch arm: "aarch64", intel: "amd64"
 
-  version '4.6.3'
+  version '4.7.0'
 
-  sha256 arm: '4e9b7ceb8039653337f51151b1a8659118b89901d16df5508b36a11e989f2abf',
-         intel: '6c442cacde5e6606d2f34384a03fe35b27421903e4dd826e4383e29b94b286df'
+  sha256 arm: '7d29ada613a50d2f59b8e5986b3698612a89ac7f80b44d707e94caac9b80c46d',
+         intel: '8f66f1f857ad0f85bbfe37bf0d59926e1e8dada6d54ec2a8ac3566b2daf691a0'
 
   url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-#{arch}-v#{version}.zip"
 
